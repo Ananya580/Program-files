2e872d6458272f67ad044981545cfa1bdcd9b4b9
Ananya580 <1nc23cs017@ncetmail.com>
2025-09-06 19:00:37 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int fact(int n)
+{
+	int f=1,i;
+	for(i=1;i<=n;i++)
+	{
+		f*=i;
+	}
+	return f;
+}
+int main()
+{
+	int n,d,sum=0,copy_n;
+	scanf("%d",&n);
+	copy_n=n;
+	//printf("%d",fact(5));
+	while(n!=0)
+	{
+		d=n%10;
+		sum+=fact(d);
+		n/=10;
+	}
+	if(copy_n==sum) printf("Its an Strong number");
+	else printf("Its  not an Strong number");
+}
